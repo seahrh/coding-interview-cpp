62d65d88f364774123f4b5e56c5dc8d7d1257686
Ruhong <seahrh@gmail.com>
2021-02-28 16:17:12 +0800

    test concert tickets

--- test/sortingandsearching/ConcertTicketsTest.cpp
+++ test/sortingandsearching/ConcertTicketsTest.cpp
@@ -4,12 +4,32 @@
 using ::testing::ElementsAre;
 using ::testing::IsEmpty;
 
+TEST(ConcertTickets, EmptyInputs)
+{
+  vector<lli> tickets;
+  vector<lli> customers;
+  EXPECT_THAT(solve(tickets, customers), IsEmpty());
+  tickets.clear();
+  customers = {1};
+  EXPECT_THAT(solve(tickets, customers), ElementsAre(-1));
+  tickets = {1};
+  customers.clear();
+  EXPECT_THAT(solve(tickets, customers), IsEmpty());
+}
+
 TEST(ConcertTickets, SingleTicket)
 {
   vector<lli> tickets = {10};
   vector<lli> customers = {10};
-  ASSERT_THAT(solve(tickets, customers), ElementsAre(10));
+  EXPECT_THAT(solve(tickets, customers), ElementsAre(10));
   tickets = {10};
   customers = {1, 10};
-  ASSERT_THAT(solve(tickets, customers), ElementsAre(-1, 10));
+  EXPECT_THAT(solve(tickets, customers), ElementsAre(-1, 10));
+}
+
+TEST(ConcertTickets, TestCase1)
+{
+  vector<lli> tickets = {5, 3, 7, 8, 5};
+  vector<lli> customers = {4, 8, 3};
+  EXPECT_THAT(solve(tickets, customers), ElementsAre(3, 8, -1));
 }

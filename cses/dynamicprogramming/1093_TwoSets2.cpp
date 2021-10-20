3b6394c9aa5fd91d7a89988cd4ceb90a539cd242
Ruhong <seahrh@gmail.com>
2021-10-20 11:19:28 +0800

    refactor two sets 2

--- cses/dynamicprogramming/1093_TwoSets2.cpp
+++ cses/dynamicprogramming/1093_TwoSets2.cpp
@@ -1,5 +1,5 @@
 /*
-Your task is to **count the number of ways** numbers 1,2,…,n can be divided into two sets of equal sum.
+Count the number of ways numbers 1,2,…,n can be divided into two sets of equal sum.
 For example, if n=7, there are four solutions:
 {1,3,4,6} and {2,5,7}
 {1,2,5,6} and {3,4,7}
@@ -17,13 +17,13 @@ Input:
 Output:
 4
 SOLUTION
-Run 0-1 knapsack to get the number of ways to reach n(n+1)4 using subsets of the numbers 1..n-1
-Why n-1? Because by only considering numbers up to n-1, we always put n in the second set,
+Run 0-1 knapsack to get the number of ways to reach n(n+1)/4 using subsets of the numbers 1..n-1
+Why n-1? Because by considering numbers up to n-1, we always put n in the second set,
 and therefore only count each pair of sets once (otherwise we count every pair of sets twice).
 dp[i][j] = number of ways to form subset sum j from the first i numbers 
-Base case: empty set to make sum 0, dp[0][0] = 1
+Base case: empty set to make sum 0, dp[i][0] = 1
 The rest of the first row, dp[0][j] = 0
-Recurring relation: dp[i][j] = dp[i - 1][j] + dp[i - 1][j - i]
+Recurrence: dp[i][j] = dp[i - 1][j] + dp[i - 1][j - i]
 Either take the ith number or not.
 Time O(N^3)
 Space O(N^3): memo table
@@ -31,30 +31,27 @@ See https://codeforces.com/blog/entry/70018
 */
 #include <bits/stdc++.h>
 using namespace std;
+const int mod = 1e9 + 7;
 
 int solve(int n)
 {
-    const int mod = 1e9 + 7;
     // triangular number
     int s = n * (n + 1) / 2;
     if (s % 2 == 1)
-    {
         return 0;
-    }
     s /= 2;
-    vector<vector<int>> dp(n, vector<int>(s + 1, 0));
-    dp[0][0] = 1;
+    vector<vector<int>> dp(n, vector<int>(s + 1));
+    // Base case
+    for (int i = 0; i < n; i++)
+        dp[i][0] = 1;
     for (int i = 1; i < n; i++)
     {
-        for (int j = 0; j <= s; j++)
+        for (int j = 1; j < s + 1; j++)
         {
-            // Not take i
+            // Already make sum j without ith coin
             dp[i][j] = dp[i - 1][j];
-            // Take i
             if (j - i >= 0)
-            {
                 (dp[i][j] += dp[i - 1][j - i]) %= mod;
-            }
         }
     }
     return dp[n - 1][s];

48f95c7a96c172d1428c3bbbec70f1d1baeec3fb
Ruhong <seahrh@gmail.com>
2023-11-05 09:59:13 +0800

    comments

--- cses/sortingandsearching/1164_RoomAllocation.cpp
+++ cses/sortingandsearching/1164_RoomAllocation.cpp
@@ -1,17 +1,17 @@
 /*
-There is a large hotel, and n customers will arrive soon. 
+There is a large hotel, and n customers will arrive soon.
 Each customer wants to have a single room.
-You know each customer's arrival and departure day. 
-Two customers can stay in the same room 
+You know each customer's arrival and departure day.
+Two customers can stay in the same room
 if the departure day of the first customer is earlier than the arrival day of the second customer.
 What is the minimum number of rooms that are needed to accommodate all customers? And how can the rooms be allocated?
 Input
 The first input line contains an integer n: the number of customers.
-Then there are n lines, each of which describes one customer. 
+Then there are n lines, each of which describes one customer.
 Each line has two integers a and b: the arrival and departure day.
 Output
 Print first an integer k: the minimum number of rooms required.
-After that, print a line that contains the room number of each customer in the same order as in the input. 
+After that, print a line that contains the room number of each customer in the same order as in the input.
 The rooms are numbered 1,2,…,k. You can print any valid solution.
 Constraints
 1≤n≤2⋅10^5
@@ -26,10 +26,12 @@ Output:
 2
 1 2 1
 SOLUTION
+Time O(N lg N): Sort customers by arrival/departure times.
+Space O(N): result array, stack to hold empty rooms.
 */
 #include <bits/stdc++.h>
 #define ll long long
-// Sort by time, then arrivals before depatures
+// Sort customers by arrival/departure times.
 #define tri tuple<ll, bool, ll>
 using namespace std;
 

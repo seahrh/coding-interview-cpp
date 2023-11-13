dd72683d9388e97f58b3c19ffc3ec4e67f5c1487
Ruhong <seahrh@gmail.com>
2023-11-13 14:38:32 +0800

    minor change

--- cses/sortingandsearching/2168_NestedRangesCheck.cpp
+++ cses/sortingandsearching/2168_NestedRangesCheck.cpp
@@ -1,10 +1,10 @@
 /*
-Given n ranges, your task is to determine for each range 
+Given n ranges, your task is to determine for each range
 if it contains some other range and if some other range contains it.
 Range [a,b] contains range [c,d] if a≤c and d≤b.
 Input
 The first input line has an integer n: the number of ranges.
-After this, there are n lines that describe the ranges. 
+After this, there are n lines that describe the ranges.
 Each line has two integers x and y: the range is [x,y].
 You may assume that no range appears more than once in the input.
 Output
@@ -24,7 +24,7 @@ Output:
 1 0 0 0
 0 1 0 1
 SOLUTION
-Sort ranges by x, y desc so that containing ranges come first.
+Sort ranges by x asc, y desc so that containing ranges come first.
 e.g. (1, 6), (2, 5), (2, 4), (3, 6), (4, 8)
 Time O(N lg N)
 Space O(N): store result array otherwise O(1)
@@ -51,31 +51,31 @@ int main()
     }
     sort(rs.begin(), rs.end());
     vector<bool> res(n);
-    ll rig = inf;
+    ll prev = inf;
     for (auto it = rs.rbegin(); it != rs.rend(); ++it)
     {
         auto [x, y, i] = *it;
         y *= -1;
-        if (y < rig)
-            rig = y;
+        if (y < prev)
+            prev = y;
         else
             // curr range contains prev range
-            // curr.x <= prev.x && prev.y <= curr.y 
+            // curr.x <= prev.x && prev.y <= curr.y
             res[i] = 1;
     }
     for (ll i = 0; i < n; i++)
         cout << res[i] << " ";
     cout << endl;
     fill(res.begin(), res.end(), 0);
-    rig = 0;
+    prev = 0;
     for (auto [x, y, i] : rs)
     {
         y *= -1;
-        if (y > rig)
-            rig = y;
+        if (y > prev)
+            prev = y;
         else
             // curr range is contained by prev range
-            // prev.x <= curr.x && curr.y <= prev.y 
+            // prev.x <= curr.x && curr.y <= prev.y
             res[i] = 1;
     }
     for (ll i = 0; i < n; i++)
